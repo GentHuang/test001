81e3c24973b566f7510bd6bbc11a59e0286988c8
huangzijun316@163.com <huangzijun316@163.com>
2016-07-01 17:51:07 +0800

    1

--- Thread/ViewController.m
+++ Thread/ViewController.m
@@ -18,7 +18,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+
+    NSLog(@"=====");
+    
+    
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
