66719cf28007acf6f1193d9b098f5bf7816f34d6
huangzijun316@163.com <huangzijun316@163.com>
2016-07-01 17:27:20 +0800

    1

---
+++ Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thread
+//
+//  Created by apple on 16/6/30.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Thread/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  Thread
+//
+//  Created by apple on 16/6/30.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <pthread.h>
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    
+    pthread_t thread;
+    
+    //创建一个线程
+//    pthread_create(&thread, NULL, start, NULL);
+    
+}
+
+
+//void *start(void *data) {
+//    
+//    
+//    
+//}
+
+@end
